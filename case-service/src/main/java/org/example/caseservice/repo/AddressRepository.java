6b86801a7cb20b07716dc2fdb2f2bef7839977d0
Barry Evans <barry_j_evans@yahoo.co.uk>
2015-12-20 18:03:16 +0000

    Configured to run on Cloud Foundry

--- case-service/src/main/java/org/example/caseservice/repo/AddressRepository.java
+++ case-service/src/main/java/org/example/caseservice/repo/AddressRepository.java
@@ -3,10 +3,16 @@ package org.example.caseservice.repo;
 import java.util.List;
 
 import org.example.caseservice.model.Address;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 
 
-public interface AddressRepository extends CrudRepository<Address, Long> {
+public interface AddressRepository extends PagingAndSortingRepository<Address, Long> {
 
-	public List<Address> findByPostcode(String postcode);
+	/**
+	 * 
+	 * @param postcode Uses the @Param to be accessible from URL
+	 * @return
+	 */
+	public List<Address> findByPostcode(@Param("postcode") String postcode);
 }
