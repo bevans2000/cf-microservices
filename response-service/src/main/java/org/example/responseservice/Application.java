bf9602d30664b9c02f3373640baf0c772ddccc91
Barry Evans <barry_j_evans@yahoo.co.uk>
2016-01-24 14:27:08 +0000

    mend

--- response-service/src/main/java/org/example/responseservice/Application.java
+++ response-service/src/main/java/org/example/responseservice/Application.java
@@ -12,7 +12,6 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  */
 @EnableDiscoveryClient
 @EnableEurekaClient
-@Enable
 @SpringBootApplication
 public class Application {
     public static void main(String[] args) {
